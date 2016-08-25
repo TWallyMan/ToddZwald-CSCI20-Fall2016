a7b396ba1f6b65ed7a7d569e0f6570ab21eaa24c
Todd Zwald <tzwald001@student.butte.edu>
2016-08-25 21:07:46 +0000

    Lab1.cpp

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   cout << "  *  " << endl;
+   cout << " *** " << endl;
+   cout << "*****" << endl;
+   cout << "               -" <<endl;
+   cout << "              l l" <<endl;
+   cout << "         -  - l l -" <<endl;
+   cout << "        l ll ll ll l___" <<endl;
+   cout << "        l              >>" <<endl;
+   cout << "        \\             /" <<endl;
+   cout << "         \\           /" <<endl;
+   cout << "          \\         /" <<endl;
+   cout << "           \\_______/" <<endl;
+    
+   return 0;
+}
+

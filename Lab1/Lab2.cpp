f1dd66bebe0972c421642504192d6608ec159c09
Todd Zwald <tzwald001@student.butte.edu>
2016-08-25 21:25:17 +0000

    Lab2.cpp

---
+++ Lab1/Lab2.cpp
@@ -0,0 +1,15 @@
+// Todd Zwald
+// 8/25/16
+// Simple Computer game that has the user guessing what number the computer chose between 1-100
+
+# include <iostream>
+using namespace std;
+int main()
+{
+    // computer choses a number between 1-100
+    // user types number in bow to determine if correct
+    // if answer is correct, give 10 points
+    // if answer is wrong, computer states higher or lower, minus 1 point and try again
+    // repeat process until either correct answer is reached or no more points are left
+    
+}

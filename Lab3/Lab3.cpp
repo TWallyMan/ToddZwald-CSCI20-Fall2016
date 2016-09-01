7833b41fae2c136cb24e7e47756efdec4ac124f0
Todd Zwald <tzwald001@student.butte.edu>
2016-09-01 07:43:08 +0000

    LAb3

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name1 = "";
+    string color1 = "";
+    string animal_1 = "";
+    string noun1 = "";
+    string noun2 = "";
+    string pluralNoun1 = "";
+    
+    cout << "What is the persons name?" <<endl;
+    cin >> name1;
+    cout << "What is your favorite color?" << endl;
+    cin >> color1;
+    cout << "What is your favorite anima?" << endl;
+    cin >> animal_1;
+    cout << ""
+}

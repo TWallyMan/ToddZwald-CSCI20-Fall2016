2e8ade1c21dd7f12b23d9986d582ea4bd22af255
Todd Zwald <tzwald001@student.butte.edu>
2016-09-20 20:26:25 +0000

    Lab6

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void CopyRight(){
+    
+    cout << "  *  " << endl;
+    cout << " *** " << endl;
+    cout << "*****" << endl;
+    cout << "               -" <<endl;
+    cout << "              l l" <<endl;
+    cout << "         -  - l l -" <<endl;
+    cout << "        l ll ll ll l___" <<endl;
+    cout << "        l              >>" << "    Todd Zwald Copyright 2016" <<endl;
+    cout << "        \\             /" <<endl;
+    cout << "         \\           /" <<endl;
+    cout << "          \\         /" <<endl;
+    cout << "           \\_______/" <<endl;
+    
+}
+
+int main (){
+    
+    CopyRight();
+    
+}

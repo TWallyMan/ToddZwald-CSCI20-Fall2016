be559153a24eb3273e55de1323ff671fa1cf3b9c
Todd Zwald <tzwald001@student.butte.edu>
2016-09-20 20:57:35 +0000

    Lab8

--- Lab8/lab8.cpp
+++ Lab8/lab8.cpp
@@ -2,22 +2,29 @@
 #include <string>
 using namespace std;
 
+// Converstion program for Kilos to pounds and Visa Versa
+//Todd Zwald
+// 9/20/16
+
+
+//Function to chage Kilos into Pounds
 double ToKilo(int A){
     
     return A / 2.2;
 }
 
+//Function to chage Pounds in to Kilos
 double ToPounds(int B){
     
     return B * 2.2;
     
 }
 
-
+// Main Program for user input to get an output for Kili/Pound conversion
  int main() {
      
-    int numPounds = 0;
-    int numKilos = 0;
+    int numPounds = 0.0;
+    int numKilos = 0.0;
      
     cout << "How many kilograms?: ";
     cin >> numKilos;

e836f373f83d7aaf2fbd401adf37f5922055ce20
Todd Zwald <tzwald001@student.butte.edu>
2016-09-20 05:36:18 +0000

    Lab7

---
+++ Lab7/Lab7.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Fallen Leaves : Billy Talent
+// Todd Zwald
+// 9/19/16
+
+
+// Repeated Chorus
+void fallenLeavesChorus (){
+    
+    cout << " " << endl;
+    cout << "In a crooked little town, they were lost and never found" << endl;
+    cout << "Fallen leaves, fallen leaves, fallen leaves... on the ground" << endl;
+    cout << "Run away before you drown, or the streets will beat you down" << endl;
+    cout << "Fallen leaves, fallen leaves, fallen leaves... on the ground" << endl;
+    cout << " " << endl;
+    
+}
+    
+    
+    
+    
+// Main song plus Chorus
+int main(){
+    
+    cout << "In a crooked little town, they were lost and never found" << endl;
+    cout << "Fallen leaves, fallen leaves, fallen leaves... on the ground" << endl;
+    cout << " " << endl;
+    cout << "I hitched a ride, until the coast" << endl;
+    cout << "To leave behind, all of my ghosts" << endl;
+    cout << "Searching for something, I couldn't find at home" << endl;
+    cout << " " << endl;
+    cout << "Can't get no job, can you spare a dime?" << endl;
+    cout << "Just one more hit, and I'll be fine" << endl;
+    cout << "I swear to God, this'll be my one last time!" << endl;
+    
+    fallenLeavesChorus();
+    
+    cout << "When it gets dark, in Pigeon Park" << endl;
+    cout << "Voice in my head, will soon be fed" << endl;
+    cout << "By the vultures, that circle round the dead!" << endl;
+    
+    fallenLeavesChorus();
+    
+    cout << "I never once thought, I'd ever be caught!" << endl;
+    cout << "Staring at sidewalks, hiding my track marks!" << endl;
+    cout << "I left my best friends, or did they just leave me?" << endl;
+    
+    fallenLeavesChorus();
+    
+    cout << "Run away before you drown!" << endl;
+    cout << "Fallen leaves, fallen leaves, fallen leaves... on the ground" << endl;
+    cout << "Run away before you drown!" << endl;
+    cout << "Fallen leaves, fallen leaves, fallen leaves... on the ground" << endl;
+    
+    return 0;
+
+}

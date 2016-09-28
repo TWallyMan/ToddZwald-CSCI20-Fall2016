9667c946dc7caa956a01377eb4e499d8b4a98913
Todd Zwald <tzwald001@student.butte.edu>
2016-09-28 06:27:45 +0000

    Lab9

---
+++ Lab9/Lab9.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+Class MUST be named TemperatureConverter
+Class variable should be kelvin_.
+You need a default constructor (sets kelvin to 0) and an overloaded constructor that accepts a kelvin
+value.
+Appropriate accessor and mutator function for kelvin as described:
+• SetTempFromKelvin – accepts a kelvin value and stores it
+• GetTempFromKelvin – returns the kelvin value
+The following functions as described
+• SetTempFromCelsius – accepts a Celsius value and converts it to kelvin Formula: k=c + 273.15
+• SetTempFromFahrenheit – accepts a Fahrenheit value and converts it to kelvin Formula: k = (5 *
+(f -32)/9) + 273.15
+• GetTempAsCelsius – returns the Celsius temperature. Formula: k – 273.15
+• GetTempAsFahrenheit – returns the Fahrenheit temperature. Formula: ((c*9)/5 + 32)
+• PrintTemperatures – prints the value as kelvin, Celsius and Fahrenheit by using the other
+functions available.
+*/
+
+
+class TemperatureConverter {
+    double
+}
+
+
+int main (){
+    
+}
